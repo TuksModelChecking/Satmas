9c2e875caeff8ec5e34ed58119e0b283c9629d24
Josua Botha <josua.public@gmail.com>
2021-08-06 13:06:48 +0200

    make sure all variable names match regex of pyeda

--- implementation/logic_encoding.py
+++ implementation/logic_encoding.py
@@ -333,7 +333,7 @@ def encode_goal(agent: Agent, time: int, total_num_agents: int) -> Or:
 def encode_action(action: str, agent: Agent, time: int) -> And:
     return binary_encode(
         to_binary_string(action_number(action), len(agent.acc)),
-        f"act({action})a{agent}t{time}"
+        f"act_{action}_a{agent}t{time}"
     )
 
 
@@ -341,7 +341,7 @@ def encode_action(action: str, agent: Agent, time: int) -> And:
 def encode_strategic_decision(action: str, agent: Agent, time: int) -> And:
     return binary_encode(
         to_binary_string(action_number(action), len(agent.acc)),
-        f"s_act({action})a{agent.id}t{time}"
+        f"s_act_{action}_a{agent.id}t{time}"
     )
 
 
