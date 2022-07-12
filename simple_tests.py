9651430f5e8f18caee26923299c7102878a299d3
Josua Botha <josua.botha@labs.epiuse.com>
2022-07-12 16:15:17 +0200

    Add I5 test

--- simple_tests.py
+++ simple_tests.py
@@ -99,6 +99,12 @@ def test_f4():
     ) is False, "F4 Should Not Be Solvable"
 
 
+def test_i5():
+    assert logic_encoding.main(
+        f"{INITIAL_PATH}/Satmas/tests/suite1/I5true.txt"
+    ) is True, "I5 Should Be Solvable"
+
+
 def test_j1():
     assert logic_encoding.main(
         f"{INITIAL_PATH}/Satmas/tests/suite1/J1false.txt"
@@ -149,19 +155,31 @@ def test_email():
 
 def r1_false():
     assert logic_encoding.main(
-        f"{INITIAL_PATH}/Satmas/tests/from/R1False.yml"
+        f"{INITIAL_PATH}/Satmas/tests/from/R1False.txt"
     ) is False, "Problem should not be solvable."
 
 
 def r2_true():
     assert logic_encoding.main(
-        f"{INITIAL_PATH}/Satmas/tests/from/R2Unused0True.yml"
+        f"{INITIAL_PATH}/Satmas/tests/from/R2Unused0True.txt"
     ) is True, "Problem should be solvable."
 
 
-def r3_unused2_true():
+def r3_unused1_true():
     assert logic_encoding.main(
-        f"{INITIAL_PATH}/Satmas/tests/from/R3Unused1True.yml"
+        f"{INITIAL_PATH}/Satmas/tests/from/R3Unused1True.txt"
+    ) is True, "Problem should be solvable."
+
+
+def r4_unused2_true():
+    assert logic_encoding.main(
+        f"{INITIAL_PATH}/Satmas/tests/from/R4Unused2True.txt"
+    ) is True, "Problem should be solvable."
+
+
+def r5_unused3_true():
+    assert logic_encoding.main(
+        f"{INITIAL_PATH}/Satmas/tests/from/R5Unused3True.txt"
     ) is True, "Problem should be solvable."
 
 
@@ -182,6 +200,7 @@ if __name__ == "__main__":
     test_f2()
     test_f3()
     test_f4()
+    test_i5()
     test_j1()
     test_j2()
     test_j3()
@@ -191,4 +210,9 @@ if __name__ == "__main__":
     test_k3()
     test_k4()
     test_email()
+    r1_false()
+    r2_true()
+    r3_unused1_true()
+    r4_unused2_true()
+    r5_unused3_true()
     print("Everything passed")
