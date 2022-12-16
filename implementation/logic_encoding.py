3cb72e3afc4fd6a2b22cba926b01cdd3b7e51afe
Josua Botha <josuabotha@proton.me>
2022-12-16 10:02:45 +0200

    test version with old solver

--- implementation/logic_encoding.py
+++ implementation/logic_encoding.py
@@ -109,42 +109,46 @@ def iterative_solve(mra: MRA, k_low: int, k_high: int) -> bool:
         solve_start = time.perf_counter()
         # print("STARTING DIMACS ENCODING")
 
-        dimacs = str(expr2dimacscnf(e)[1]).split("\n")
-        # numbers = extract_var_numbers(e.encode_cnf()[0], "nu_r")
-        # print("AUX Variable Name Number Pairs:")
-        # print(numbers)
-        # count = 1
-        # for n in numbers:
-        #     dimacs.append(f"{count} {n} 0\n")
-        #     if INCREMENTAL_SOFT_CLAUSE_COST:
-        #         count += 1
-        wdimacs = harden_clauses(dimacs)
-        file = open("dimacs.txt", "w")
-        file.writelines(wdimacs)
-        file.close()
+        # dimacs = str(expr2dimacscnf(e)[1]).split("\n")
+        # # numbers = extract_var_numbers(e.encode_cnf()[0], "nu_r")
+        # # print("AUX Variable Name Number Pairs:")
+        # # print(numbers)
+        # # count = 1
+        # # for n in numbers:
+        # #     dimacs.append(f"{count} {n} 0\n")
+        # #     if INCREMENTAL_SOFT_CLAUSE_COST:
+        # #         count += 1
+        # wdimacs = harden_clauses(dimacs)
+        # file = open("dimacs.txt", "w")
+        # file.writelines(wdimacs)
+        # file.close()
         # print("DIMACS ENCODING DONE")
         print("---")
         print("STARTING EXTERNAL SOLVER")
-        p = subprocess.run(['./open-wbo_release', 'dimacs.txt'], stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
 
-        if PRINT_FULL_SOLUTION:
-            print(str(p.stdout))
-        wbo_printout = str(p.stdout).split("\\ns")
-        s = wbo_printout.pop()[1:14]
-        print(s)
-        solved = s == "OPTIMUM FOUND"
-        if solved:
-            # print(wbo_printout)
-            n_s_solved = wbo_printout.pop().split("\\no ")[-2:]
-            # print(f"Num soft clauses: {len(numbers)}")
-            # print(f"Nums at end: {n_s_solved[1]}{f' and {n_s_solved[0]}' if len(n_s_solved[0]) < 10 else ''}")
-            print(f"Sum of cost of used resources {int(n_s_solved[1])}")
-        print("---")
-        # print("\nENDING EXTERNAL SOLVER")
-        # print_solution_path(e)
+        # p = subprocess.run(['./open-wbo_release', 'dimacs.txt'], stdout=subprocess.PIPE,
+        #                    stderr=subprocess.PIPE)
         #
-        # solved = solve(e)
+        # if PRINT_FULL_SOLUTION:
+        #     print(str(p.stdout))
+        # wbo_printout = str(p.stdout).split("\\ns")
+        # s = wbo_printout.pop()[1:14]
+        # print(s)
+        # solved = s == "OPTIMUM FOUND"
+
+        solved = e.satisfy_one()
+
+        # if solved:
+        #     # print(wbo_printout)
+        #     n_s_solved = wbo_printout.pop().split("\\no ")[-2:]
+        #     # print(f"Num soft clauses: {len(numbers)}")
+        #     # print(f"Nums at end: {n_s_solved[1]}{f' and {n_s_solved[0]}' if len(n_s_solved[0]) < 10 else ''}")
+        #     print(f"Sum of cost of used resources {int(n_s_solved[1])}")
+        # print("---")
+        # # print("\nENDING EXTERNAL SOLVER")
+        # # print_solution_path(e)
+        # #
+        # # solved = solve(e)
         solve_end = time.perf_counter()
         print(f"    s_t = {round(solve_end - solve_start, 1)}s\n      sat: {'TRUE' if solved else 'false'}")
         total_encoding_time += encoding_end - encoding_start
@@ -684,7 +688,7 @@ def main(given_path):
 if __name__ == "__main__":
     INCREMENTAL_SOFT_CLAUSE_COST = True
     PRINT_FULL_SOLUTION = False
-    path = "/home/josua/Development/Satmas/tests/paper/opt8a8r_1.yml"
+    path = "/home/josua/Development/Satmas/tests/feedback/a10r10d2c5b50.yml"
     if len(sys.argv) >= 2:
         path = sys.argv[1]
     print(path.split("/").pop())
