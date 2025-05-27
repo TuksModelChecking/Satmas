516f34187435507e7bd6f446487b7343621fc8e5
Steven Jordaan <steven.j.jordaan2000@gmail.com>
2025-05-27 07:00:36 +0200

    Simplify definition 12

--- src/encoding/SBMF_2021/definition_12.py
+++ src/encoding/SBMF_2021/definition_12.py
@@ -14,23 +14,8 @@ from .definition_17 import encode_resource_state_at_t
 # \end{definition}
 
 def encode_initial_state(mra_problem: MRA, num_agent_states: int) -> Formula:
-    
-    resource_unallocated_clauses = []
-    UNALLOCATED_AGENT_ID = 0  # Represents a_0 in the definition [r = a_0]_0
-    INITIAL_TIMESTEP = 0      # Represents the time step _0 in [r = a_0]_0
-    Res = mra_problem.res     # Renamed Res to mra_problem.res for clarity
 
-    # For each resource r in Res (mra_problem.res)
-    for r in Res:
-        # Append the encoding of [r = a_0]_0
-        # This means resource r is assigned to the unallocated agent (a_0) at time step 0.
-        clause = encode_resource_state_at_t(
-            r,                   # r
-            UNALLOCATED_AGENT_ID,    # a_0
-            INITIAL_TIMESTEP,        # _0
-            num_agent_states         # Contextual parameter for encode_resource_state
-        )
-        resource_unallocated_clauses.append(clause)
-    
-    # Return the conjunction \bigwedge_{r \in Res} [r = a_0]_0
-    return And(*[clause for clause in resource_unallocated_clauses if clause is not None])
+    return And(*(
+        encode_resource_state_at_t(r,0,0,num_agent_states)
+        for r in mra_problem.res
+    ))
